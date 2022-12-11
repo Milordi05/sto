3ff6387aa91ae635a0a7818beaadca45c4d8c90a
Milordi05 <110412290+Milordi05@users.noreply.github.com>
2022-12-11 10:43:19 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,17 +1,17 @@
 const products = [
 	{
 		id1: 1,
-		notice: " هنا اي اعلان" ,
-	   title1: "عنوان 1"  , 
-	   title2: " عنوان 2" , 
-	   category1_title: 'كل المنتجات' ,
-	   category2_title: '  صحة' ,
-	   category3_title: ' النظافة ' ,
-	   category4_title: 'رقم 4 ' ,
-	   category5_title: 'رقم 5 ', 
+		notice: " قدم طلبك مباشرة عبر الهاتف 0664.59.57.83" ,
+	   title1: " "  , 
+	   title2: " " , 
+	   category1_title: '' ,
+	   category2_title: '  ' ,
+	   category3_title: '  ' ,
+	   category4_title: '  ' ,
+	   category5_title: '  ', 
 		link_facebook: 'https://www.facebook.com/',
         link_instegram: 'https://www.facebook.com/',
-		nember: 'tel:0000000',
+		nember: 'tel:0664595783',
 		Adrar:1200, Chlef:800, Laghouat:700, Oum_El_Bouaghi:600, Batna:600, Béjaïa:600, Biskra:600, Béchar:800,Blida:600,Bouira:600,Tamanrasset:600,Tébessa:600,Tlemcen:600,Tiaret:600,Tizi_Ouzou:600,Alger:600,Djelfa:600,Jijel:600,Sétif:600,Saïda:600,Skikda:600,Sidi_Bel_Abbès:600,Annaba:600,Guelma:600,Constantine:600,Médéa:600,Mostaganem:600,MSila:600,Mascara:600,Ouargla:600,Oran:600,El_Bayadh:600,Illizi:600,Bordj_Bou_Arreridj:600,Boumerdès:600,El_Tarf:600,Tindouf:600,Tissemsilt:600,El_Oued:600,Khenchela:600,Souk_Ahras:600,Tipaza:600,Mila:600,Aïn_Defla:600,Naâma:800,Aïn_Témouchent:600,Ghardaïa:600,Relizane:600,Timimoun:600,Bordj_Badji_Mokhtar:600,Ouled_Djellal:600,Beni_Abbes:600,In_Salah:600,In_Guezzam:600,Touggourt:600,Djanet:600,El_MGhair:600,El_Meniaa:600,
 		
 		
@@ -22,11 +22,16 @@ const products = [
 		notice: " جديد",
 		image: 'product1/img/1.png',image2: 'img/2.png',image3: 'img/3.png',image4: 'img/4.png',image5: 'img/5.png',image6: 'img/6.png',
 		link: 'product1/index.html' ,
-		category: 'cat2',
+		category: '',
 		title: ' منشر الملابس' ,
-		description: ' منشر الملابس للاستعمال في اماكن عديدة '  ,
+		description: ' للطلب أدخل معلوماتك في الخانات أسفله '  ,
 		price1:'', price: 5900, percentage: 0 ,
-		description2: ' مصنوع من الحديد عالي الجودة و تم دهنه بطلاء مضاد للرطوبة و الصدأ الاسلاك مصنوعة من الالمنيوم الذي يتميز بوزنه الخفيف و مقاومته للماءو الرطوبة سمكه لا يتجاوز 3 سم حيث سيوفر لك الكثير من المساحةكما ان تصميمه جميل و سيعطي لمسة ابداعية في ديكور بيتك'  ,
+		description2: '  ✅ طوله 98 سم و عرضه 12 سم
+✅ سمكه لا يتجاوز 5 .3 سم حيث سيوفر لك الكثير من المساحة كما ان تصميمه جميل و سيعطي 
+      لمسة ابداعية في ديكور بيتك
+✅ يحتوي على 5 أسلاك لنشر الملابس طول كل واحد 96 سم يعني ستكون عندك مساحة  4,8 متر لنشر الملابس
+✅ مصنوع من الحديد عالي الجودة و تم دهنه بطلاء مضاد للرطوبة و الصدأ 
+✅ الاسلاك مصنوعة من الحديد المضاد للصدأ و مغلفة بالبلاستيك '  ,
 	},
 	{
 	
